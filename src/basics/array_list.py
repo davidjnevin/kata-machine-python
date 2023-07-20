1e3aa5fdf5e0eaa3742b0d440cf320a988b53aa5
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-07-21 00:41:06 +0200

    Add ArrayList resolves #1

--- src/basics/array_list.py
+++ src/basics/array_list.py
@@ -1,22 +1,77 @@
 class ArrayList:
-    def __init__(self):
-        self.length = 0
+    def __init__(self, capacity=10):
+        self.capacity: int = capacity  # maximum capacity
+        self.data = [None] * capacity  # allocate memory
+        self.length: int = 0  # initial length
+
+    def __len__(self):
+        return self.length
+
+    def __str__(self) -> str:
+        return str([item for item in self.data])
+
+    def _isFull(self) -> bool:
+        isFull = False
+        if self.length >= self.capacity:
+            isFull = True
+        return isFull
+
+    def _resize(self) -> None:
+        self.capacity *= 2
+        extended_array_list = [None] * self.capacity
+        for i in range(self.length):
+            extended_array_list[i] = self.data[i]
+        self.data = extended_array_list
+
+    def _isValidIndex(self, idx) -> bool:
+        isValidIndex = True
+        if idx < 0 or idx >= self.length:
+            raise IndexError("Invalid index")
+        return isValidIndex
 
     def prepend(self, item):
-        return NotImplemented
+        if self.length + 1 > self.capacity:
+            self._resize()
+        keepgoing = True
+        last_idx = self.length
+        while(keepgoing):
+            self.data[last_idx] = self.data[last_idx - 1]
+            last_idx -= 1
+            if last_idx == 0:
+                keepgoing = False
+        self.data[0] = item
+        self.length += 1
 
     def append(self, item):
-        return NotImplemented
+        if self._isFull():
+            self._resize()
+        self.data[self.length] = item
+        self.length += 1
 
     def insert_at(self, item, idx):
-        return NotImplemented
+        if self._isFull():
+            self._resize()
 
     def remove(self, item):
-        return NotImplemented
+        found = False
+        # better search possibilities
+        for idx, element in enumerate(self.data):
+            if element == item:
+                found = True
+                self.remove_at(idx)
+        if not found:
+            raise ValueError(f"{item} not found")
 
     def remove_at(self, idx):
-        return NotImplemented
+        if self._isValidIndex(idx):
+            # Shift all items left overwriting idx item
+            for i in range(idx, self.length):
+                self.data[i] = self.data[i+1]
+            self.length -= 1
 
     def get(self, idx):
-        return NotImplemented
+        if self._isValidIndex(idx):
+            return self.data[idx]
+
+
 
--- tests/basics/test_array_list.py
+++ tests/basics/test_array_list.py
@@ -1,5 +1,141 @@
-import ..ArrayList as arraylist
+from typing import cast
+
 import pytest
+from _pytest.python_api import raises
+
+import src.basics.array_list as array_list
+
 
 def test_append():
-    assert arraylist.append(1)
+    a = array_list.ArrayList(capacity=10)
+    a.append(1)
+    assert a.length == 1
+    a.append(2)
+    assert a.length == 2
+
+
+def test_resize():
+    a = array_list.ArrayList(capacity=10)
+    for i in range(10):
+        a.append(i)
+
+    assert a.length == 10
+    assert a.capacity == 10
+
+    a.append(11)
+    assert a.length == 11
+    assert a.capacity == 20
+
+
+def test_get():
+    a = array_list.ArrayList(capacity=10)
+    for i in range(10):
+        a.append(i)
+
+    assert a.get(0) == 0
+    assert a.get(9) == 9
+
+
+def test_get_invalid_index_wher_index_is_greater_than_length():
+    a = array_list.ArrayList(capacity=10)
+
+    for i in range(5):
+        a.append(i)
+    with pytest.raises(IndexError):
+        assert a.get(5)
+
+
+def test_get_invalid_index_wher_index_is_negative():
+    a = array_list.ArrayList(capacity=10)
+
+    for i in range(5):
+        a.append(i)
+    with pytest.raises(IndexError):
+        assert a.get(-1)
+
+
+def test_prepend():
+    a = array_list.ArrayList(capacity=10)
+
+    for i in range(5):
+        a.append(i)
+
+    array_length = a.length
+
+    a.prepend(11)
+    assert a.get(0) == 11
+
+    # check all values exist at the correct index
+    assert a.data == [11, 0, 1, 2, 3, 4, None, None, None, None]
+
+    assert a.length == array_length + 1
+
+
+def test_prepend_to_full_array():
+    a = array_list.ArrayList(capacity=10)
+
+    # Fill the first ten available slots
+    for i in range(10):
+        a.append(i)
+
+    array_length = a.length
+    assert array_length == 10
+    a.prepend(10)
+    assert a.get(0) == 10
+
+    # check all values exist at the correct index
+    assert a.data == [10, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, None, None, None, None, None, None, None, None, None]
+
+    assert a.length == array_length + 1
+
+    assert a.capacity == 20
+
+
+def test_remove_at_index_reduces_length():
+    a = array_list.ArrayList(capacity=10)
+    for i in range(5):
+        a.append(i)
+
+    a.remove_at(0)
+    assert a.length == 4
+
+
+def test_remove_at_index_removes_item_at_index():
+    a = array_list.ArrayList(capacity=10)
+    for i in range(5):
+        a.append(i)
+
+    a.remove_at(0)
+    for i in range(a.length):
+        assert a.get(i) == i + 1
+
+
+def test_remove_at_invalid_index_returns_IndexError():
+    a = array_list.ArrayList(capacity=10)
+    for i in range(5):
+        a.append(i)
+
+    with pytest.raises(IndexError):
+        a.remove_at(5)
+
+
+def test_remove_item_that_exists_in_arraryList():
+    a = array_list.ArrayList(capacity=10)
+    for i in range(5):
+        a.append(i)
+
+    a.remove(3)
+    assert a.length == 4
+    assert str(a) == '[0, 1, 2, 4, None, None, None, None, None, None]'
+
+
+def test_remove_item_that_does_not_exist_in_arraryList():
+    a = array_list.ArrayList(capacity=10)
+    for i in range(5):
+        a.append(i)
+    original_length = a.length
+
+    with pytest.raises(ValueError, match="77 not found"):
+        a.remove(77)
+    assert a.length == original_length
+    assert str(a) == '[0, 1, 2, 3, 4, None, None, None, None, None]'
